060a7281b36493e39862fd09b8206e0301657df9
Santeri67 <bhd483@myy.haaga-helia.fi>
2024-02-12 14:27:37 +0200

    Ensimmäinen commit

---
+++ src/main/java/controller/BookController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
