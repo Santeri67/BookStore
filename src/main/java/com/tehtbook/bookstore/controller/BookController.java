13f07f70e2b2993a4c3274b062306886ee148799
Santeri67 <bhd483@myy.haaga-helia.fi>
2024-02-26 12:47:19 +0200

    bookstoreListpaivitus

--- src/main/java/com/tehtbook/bookstore/controller/BookController.java
+++ src/main/java/com/tehtbook/bookstore/controller/BookController.java
@@ -1,14 +1,28 @@
 package com.tehtbook.bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tehtbook.bookstore.model.Book;
+import com.tehtbook.bookstore.repository.BookRepository;
+
 @Controller
 public class BookController {
 
-    @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+    private final BookRepository bookRepository;
+
+    // Konstruktori-injektointi BookRepositorylle
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
 }
