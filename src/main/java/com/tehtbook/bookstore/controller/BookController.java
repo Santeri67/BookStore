d6c2bc448b59a23f9d1bd00f7755213b6b0b2de6
Santeri67 <bhd483@myy.haaga-helia.fi>
2024-02-26 15:14:14 +0200

    bookstorestylesupdate

--- src/main/java/com/tehtbook/bookstore/controller/BookController.java
+++ src/main/java/com/tehtbook/bookstore/controller/BookController.java
@@ -28,22 +28,33 @@ public class BookController {
         model.addAttribute("books", books);
         return "booklist";
     }
-@GetMapping("/addbook")
-public String showAddBookForm(Model model) {
-    model.addAttribute("book", new Book());
-    return "addbook";
+    @GetMapping("/addbook")
+        public String showAddBookForm(Model model) {
+            model.addAttribute("book", new Book());
+            return "addbook";
 }
-@PostMapping("/addbook")
-public String addBook(@ModelAttribute Book book) {
-    bookRepository.save(book);
-    return "redirect:/booklist";
+    @PostMapping("/addbook")
+        public String addBook(@ModelAttribute Book book) {
+            bookRepository.save(book);
+            return "redirect:/booklist";
 }
-@GetMapping("/delete/{id}")
-public String deleteBook(@PathVariable("id") Long bookId) {
-    if (bookId != null) {
-        bookRepository.deleteById(bookId);
+    @GetMapping("/delete/{id}")
+        public String deleteBook(@PathVariable("id") Long bookId) {
+            if (bookId != null) {
+                bookRepository.deleteById(bookId);
     }
     return "redirect:/booklist";
 }
-
+    @GetMapping("/edit/{id}")
+        public String showEditBookForm(@PathVariable("id") Long bookId, Model model) {
+            Book book = bookRepository.findById(bookId).orElse(null);
+            model.addAttribute("book", book);
+            return "editbook";
+}
+    @PostMapping("/editbook")
+        public String editBook(@ModelAttribute Book book) {
+    // Oletetaan, että kirjalla on jo 'id' asetettu, joten tämä päivittää olemassa olevan kirjan
+            bookRepository.save(book);
+            return "redirect:/booklist";
+}
 }
